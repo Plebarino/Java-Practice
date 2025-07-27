f503ecd0ce79d9af180cc8529ff913a2da35e739
Plebarino <113859478+Plebarino@users.noreply.github.com>
2025-07-27 19:17:22 +0000

    LAB6 STARTED - 1ST PUSH

---
+++ PFII/LAB6/Factorials.java
@@ -0,0 +1,12 @@
+package PFII.LAB6;
+import java.util.Scanner;
+
+public class Factorials
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+       
+        input.close(); // good practice to close the scanner
+    }
+}
