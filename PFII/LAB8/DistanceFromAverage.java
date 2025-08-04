e491d4d8a69253455aa68b6cacdeaddb4cd0c876
Plebarino <113859478+Plebarino@users.noreply.github.com>
2025-08-04 04:00:53 +0000

    lab8 - first push

---
+++ PFII/LAB8/DistanceFromAverage.java
@@ -0,0 +1,57 @@
+package PFII.LAB8;
+import java.util.Scanner;
+
+public class DistanceFromAverage
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        boolean keepRunning = true;
+        
+        double[] values = new double[15];
+        int index = 0;
+
+        while (keepRunning && index < values.length)
+        {
+            System.out.print("Enter a numeric value or 99999 to quit. >> ");
+            double value = input.nextDouble();
+
+            if (value == 99999)
+            {
+                if (index == 0)
+                {
+                    System.out.println("ERROR. User input nothing.");
+                }
+                else
+                {
+                    avrg(values,index);                  
+                }
+                keepRunning = false;
+            }
+            else
+            {
+                values[index] = value;
+                index++;
+                if (index == 15)
+                {
+                    avrg(values,index);     
+                }
+            }
+        }        
+    input.close();
+    }
+
+    public static void avrg(double[] values, int index)
+    {
+        double sum = 0.0;
+        // for (double num : values)
+        for (int i =0; i < index; i++)
+        {
+            sum += values[i];
+        }
+
+        double average = sum / index;
+
+        System.out.println("You enetred " + index + " number(s) and their avergae is " + average);
+    }
+}
